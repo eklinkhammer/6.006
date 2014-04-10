df8804004b0b120e54a1f41c456e0ada860a574c
Eric Klinkhammer <eklinkhammer@gmail.com>
2014-04-10 00:13:52 -0700

    Modified PSET4.py

--- ps4/pset4/partB/PSET4.py
+++ ps4/pset4/partB/PSET4.py
@@ -1,5 +1,18 @@
 from decimal import Decimal
-
+from math import log as log
+import math
+# Find the cube root of 7
+def partA( val, guess ):
+  x0 = guess
+  i = 0
+  while ( i < 10 ):
+    xn = (1.0/3)* (val / (x0 * x0 ) + 2 * x0 )
+    x0 = xn
+    print(xn)
+    i = i + 1
+    
+if (__name__ == "__main__" ):
+  partA(7,2)
 
 # Returns the next guess after one iteration of Newton's method.
 #
@@ -10,7 +23,7 @@ def CubeRootNewtonIteration(number, guess):
   assert type(number) == int
 
   # BEGIN STUDENT CODE
-  raise NotImplementedError()
+  return (1 / 3.0 ) * ( number / ( guess * guess ) + 2 * guess )
   # END STUDENT CODE
 
 
@@ -22,7 +35,8 @@ def CubeRootGuess(number):
   assert type(number) == int
 
   # BEGIN STUDENT CODE
-  raise NotImplementedError()
+  # Completely lost here
+  return int( math.pow( number, 1/3.0 ))
   # END STUDENT CODE
 
 
@@ -34,7 +48,14 @@ def FloatCubeRoot(number):
   assert type(number) == int
 
   # BEGIN STUDENT CODE
-  raise NotImplementedError()
+  target_precision = log(number, 2) + 4 # Breaks if 3 or lower
+  current = 0
+  guess = CubeRootGuess(number) * 1.0
+
+  while ( current < target_precision ):
+    current = current + 1
+    guess = ( number / ( guess * guess) + 2 * guess ) / 3
+  return guess
   # END STUDENT CODE
 
 
@@ -46,7 +67,10 @@ def ReciprocalGuess(number):
   assert type(number) == Decimal
 
   # BEGIN STUDENT CODE
-  raise NotImplementedError()
+  # From Kevin on Piazza
+  x = number.shorten(1)
+  longX = long(x.significand)
+  return Decimal( long(1.0 / longX * 10), ( -1 * x.exponent) -1, number.precision)
   # END STUDENT CODE
 
 
@@ -59,10 +83,16 @@ def DecimalReciprocal(number):
   assert type(number) == Decimal
 
   # BEGIN STUDENT CODE
-  raise NotImplementedError()
+  guess = ReciprocalGuess(number)
+  calculations_needed = log(number.precision, 2) + 1 # Breaks without 1 sometimes
+  index = 0
+  while( index < calculations_needed ):
+    index = index + 1
+    guess =  guess * ( 2 - number * guess )
+  return guess
   # END STUDENT CODE
 
-
+  
 # Returns the best approximation to the cube root of the number using decimal
 # numbers with the given number of digits of precision.
 #
@@ -75,7 +105,19 @@ def DecimalCubeRoot(number, precision):
 
   # Note: You will need to pass reciprocal=DecimalReciprocal when creating
   #       Decimal numbers with support for division.
-
+  
   # BEGIN STUDENT CODE
-  raise NotImplementedError()
+  fudge_factor = 3
+  guess = CubeRootGuess(number)
+  target_precision = log( precision, 2) + fudge_factor
+  current = 0
+  decimal_guess = Decimal(guess, precision=precision + 100, reciprocal=DecimalReciprocal)
+  decimal_number = Decimal(number, precision=precision + 100, reciprocal=DecimalReciprocal)
+  decimal_number_div = decimal_number / 3
+  decimal_two_thirds = Decimal(2, precision=precision + 100, reciprocal=DecimalReciprocal)
+  decimal_two_thirds /= 3
+  while ( current < target_precision ):
+    current += 1
+    decimal_guess = decimal_number_div / ( decimal_guess * decimal_guess) + decimal_two_thirds * decimal_guess
+  return decimal_guess.shorten(precision)
   # END STUDENT CODE
