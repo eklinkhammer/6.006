742206d186997d84466f802eb2b0cfd06db12a8d
Eric Klinkhammer <eklinkhammer@gmail.com>
2014-05-05 12:13:43 -0700

    Pset 6 init (plus some swap files from 4?

---
+++ ps6/part_b/PSET6.py
@@ -0,0 +1,16 @@
+from Request import *
+
+# BEGIN STUDENT CODE
+####################
+
+# create_schedule
+# input: requests := unordered list of Request objects
+# output: schedule := unordered list of Request objects, a sub-set of requests
+#     such that the requests inn the schedule are non-overlapping
+#     and the total time slots are maximized
+def create_schedule(requests):
+  schedule = []
+  return schedule
+  
+####################
+# END STUDENT CODE
